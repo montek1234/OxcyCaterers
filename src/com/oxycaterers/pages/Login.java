94ac8479ad963110d4d142bdc0013c3c6d620f3b
iccs <montek.software@gmail.com>
2018-07-09 16:00:22 +0530

    third commit

--- src/com/oxycaterers/pages/Login.java
+++ src/com/oxycaterers/pages/Login.java
@@ -1,5 +1,62 @@
 package com.oxycaterers.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Login
 {
+    WebDriver driver;
+
+    @FindBy(xpath = "//input[@name='user_username']")
+    public WebElement txtUserName;
+    //WebElement txtemail = driver.findElement(By.name("email"));  //This is equivalent to above two statement
+
+    @FindBy(xpath = "//input[@name='user_password']")
+    public WebElement txtPassword;
+
+    @FindBy(xpath = "//input[@value='Login']")
+    public WebElement btnLogin;
+
+    public Login(WebDriver driver)
+    {
+       this.driver=driver;
+        PageFactory.initElements(driver,this);
+
+    }
+
+    public void setTxtUserName(String userName)
+    {
+        txtUserName.sendKeys(userName);
+    }
+
+
+    public void setTxtPassword(String password)
+    {
+        txtPassword.sendKeys(password);
+    }
+
+
+    public void clickLogin()
+    {
+        btnLogin.click();
+    }
+
+
+   /* @FindBy(xpath = "//h1[text()='Login']")
+    public WebElement lblLogin;
+
+
+
+    @FindBy(xpath = "//label[text()='Email']")
+    public WebElement lblEmail;
+
+
+    @FindBy(xpath = "//label[text()='Password']")
+    public WebElement lblPassword;
+
+
+    @FindBy(xpath = "//a[contains(text(),'I forgot')]")
+    public WebElement btnForgotPassword;*/
 }
--- src/com/oxycaterers/utilities/Driver.java
+++ src/com/oxycaterers/utilities/Driver.java
@@ -1,5 +1,6 @@
 package com.oxycaterers.utilities;
 
+
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver
 {
-    private static WebDriver MyDriver;
+    private static WebDriver myDriver;
 
     private static  long timeout=10;
     public enum DriverTypes
@@ -26,31 +27,31 @@ public class Driver
         {
             switch(d)
             {
-                case INTERNETEXPLORER:
+               /* case INTERNETEXPLORER:
                     System.setProperty("webdriver.ie.driver","D:\\Vaibhav\\D Drive\\Vaibhav Testing\\Selenium Programme\\OxcyCaterers\\Drivers\\chromedriver.exe");
                     myDriver =new InternetExplorerDriver();
-                    break;
+                    break;*/
 
                 case CHROME:
                     System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
                     myDriver =new ChromeDriver();
                     break;
 
-                case FIREFOX:
+                /*case FIREFOX:
                     System.setProperty("webdriver.gecko.driver","Drivers/geckoDriver");
                     myDriver =new FireFoxDriver();
-                    break;
+                    break;*/
             }
 
-          /*  myDriver.manage().timesouts().implycitlyWait(20, TimeUnit.SECONDS);
+            myDriver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
             myDriver.manage().window().maximize();
 
-            defaultDriver = d;
+
             // LOGGER.debug("Setting Browser to " + d);
             myDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
             // LOGGER.debug("Setting Driver Time Out to 20 Seconds");
             myDriver.manage().window().maximize();
-            //  LOGGER.debug("Maximising Browser Window");*/
+            //  LOGGER.debug("Maximising Browser Window");
         }
         timeout=10;
         //  SetBrowserLanguage(l);
